855c437fff67690ef59469ab73b3c349ad4b46e6
wolfieorama <wachirawamunyi@gmail.com>
2015-07-26 00:46:55 +0300

    added products controller and an array

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
 (function(){
     var app = angular.module("store", []);
     app.controller("storecontroller", function(){
-        this.product = gem;
+        this.products = gems;
     });
 
-    var gem = {
+    var gems = [
+
+    {
         name: "Dodecahedro",
         price: 2.95,
-        description: "In geometry, a dodecahedron is any polyhedron with twelve flat faces. The most familiar dodecahedron is the regular dodecahedron, which is a Platonic solid"
+        description: "In geometry, a dodecahedron is any polyhedron with twelve flat faces. The most familiar dodecahedron is the regular dodecahedron, which is a Platonic solid",
+        canPurchase: true,
+        soldOut: false
+    },
+    {
+        name: "Ruby",
+        price: 3.95,
+        description: "In geometry, a Ruby gemn is any polyhedron with twelve flat faces. The most familiar dodecahedron is the regular dodecahedron, which is a Platonic solid",
+        canPurchase: true,
+        soldOut: false
     }
+        ];
 
 })()
 
