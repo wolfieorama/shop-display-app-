d97678b325f68039b547af85de77fcc2c02c3637
wolfieorama <wachirawamunyi@gmail.com>
2015-07-26 00:10:54 +0300

    added store controller and objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module("store", []);
+(function(){
+    var app = angular.module("store", []);
+    app.controller("storecontroller", function(){
+        this.product = gem;
+    });
+
+    var gem = {
+        name: "Dodecahedro",
+        price: 2.95,
+        description: "In geometry, a dodecahedron is any polyhedron with twelve flat faces. The most familiar dodecahedron is the regular dodecahedron, which is a Platonic solid"
+    }
+
+})()
+
